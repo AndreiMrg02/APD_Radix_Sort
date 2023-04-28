5c7835c002dcb5f3ef9984d1d700bb35d40af491
AndreiMrg02 <murguandrei2@gmail.com>
2023-04-28 19:43:09 +0300

    chore: added inputs and outputs

--- Radix_Sort/RadixSortSeq.cpp
+++ Radix_Sort/RadixSortSeq.cpp
@@ -7,6 +7,9 @@
 #include <random>
 #include <vector>
 #include <unordered_set>
+#include <Windows.h>
+#include <psapi.h>
+#pragma comment(lib, "psapi.lib")
 
 using namespace std;
 using namespace chrono;
@@ -47,25 +50,24 @@ void radix_sort(vector<int>& arr) {
     output.clear();
     vector<int>().swap(output);
 }
-
-
+//10
+//100
+//1000
+//10000
+//100000
+//200000
+//500000
+//1000000
+//5000000
+//10000000
 int main() {
-    const int n = 100;
+    const int n = 10000000;
 
     random_device rd;
     mt19937 generator(rd());
-    uniform_int_distribution<int> distribution(1, 5000000);
+    uniform_int_distribution<int> distribution(1, 50000000);
 
-    ofstream fout("numbers.txt");
-    unordered_set<int> values;
-    while (values.size() < n) {
-        int num = distribution(generator);
-        values.insert(num);
-        fout << num << " ";
-    }
-    fout.close();
-
-    ifstream fin("numbers.txt");
+    ifstream fin("inputs/input10.txt");
     vector<int> arr;
     arr.reserve(n);
     int num;
@@ -78,10 +80,15 @@ int main() {
     radix_sort(arr);
     auto end_time = high_resolution_clock::now();
 
+    PROCESS_MEMORY_COUNTERS_EX pmc;
+    GetProcessMemoryInfo(GetCurrentProcess(), (PROCESS_MEMORY_COUNTERS*)&pmc, sizeof(pmc));
+    SIZE_T virtual_mem_used_by_process = pmc.PrivateUsage;
+
+    cout << "Memoria virtuala utilizata de proces: " << virtual_mem_used_by_process << " octeti" << endl;
     auto duration = duration_cast<microseconds>(end_time - start_time);
     double total_time = double(duration.count()) / 1000000;
 
-    ofstream foutresult("out.out");
+    ofstream foutresult("outputs/output10.out");
     for (int i = 0; i < n; i++) {
         foutresult << arr[i] << endl;
     }
