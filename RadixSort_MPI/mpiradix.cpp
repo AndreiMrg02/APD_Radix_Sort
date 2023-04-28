8c1f7a46d39bca10df0dfb453268c57ded7b0a09
AndreiMrg02 <murguandrei2@gmail.com>
2023-04-28 23:22:55 +0300

    update: added inputs

--- RadixSort_MPI/mpiradix.cpp
+++ RadixSort_MPI/mpiradix.cpp
@@ -1,68 +1,107 @@
-﻿#include <mpi.h>
-#include <iostream>
+﻿#include <algorithm>
+#include <cstring>
+#include <chrono>
+#include <iomanip>
+#include <fstream>
+#include <random>
 #include <vector>
-#include <algorithm>
+#include <unordered_set>
+#include <Windows.h>
+#include <psapi.h>
+#include <mpi.h>
+#include <iostream>
+#pragma comment(lib, "psapi.lib")
 
 using namespace std;
+using namespace chrono;
 
-// Functie de sortare a bucăților de date
+
 void radix_sort(vector<int>& arr) {
-    const int base = 10; // Baza de numărare
     int max_val = *max_element(arr.begin(), arr.end());
-    for (int exp = 1; max_val / exp > 0; exp *= base) {
-        vector<int> count(base, 0); // Contorizează frecvența cifrelor
-        vector<int> output(arr.size(), 0); // Vector pentru elementele sortate
-        // Contorizează frecvența cifrelor
+    vector<int> output(arr.size());
+    vector<int> count(10);
+
+    bool more_digits = true;
+    for (int exp = 1; more_digits; exp *= 10) {
+        fill(count.begin(), count.end(), 0);
+
+        more_digits = false;
         for (int i = 0; i < arr.size(); i++) {
-            count[(arr[i] / exp) % base]++;
+            int digit = (arr[i] / exp) % 10;
+            count[digit]++;
+            if (arr[i] >= exp * 10) {
+                more_digits = true;
+            }
         }
-        // Calculează pozițiile relative ale elementelor în vectorul sortat
-        for (int i = 1; i < count.size(); i++) {
+
+        for (int i = 1; i < 10; i++) {
             count[i] += count[i - 1];
         }
-        // Plasează elementele în vectorul sortat
+
         for (int i = arr.size() - 1; i >= 0; i--) {
-            output[count[(arr[i] / exp) % base] - 1] = arr[i];
-            count[(arr[i] / exp) % base]--;
+            int digit = (arr[i] / exp) % 10;
+            output[count[digit] - 1] = arr[i];
+            count[digit]--;
         }
-        // Copiază vectorul sortat în vectorul original
-        for (int i = 0; i < arr.size(); i++) {
-            arr[i] = output[i];
-        }
+
+        arr = output;
     }
+
+    // Eliberam memoria alocata dinamic
+    output.clear();
+    vector<int>().swap(output);
 }
 
 int main(int argc, char** argv) {
     MPI_Init(&argc, &argv);
+    int world_rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
 
-    int rank, size;
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    int world_size;
+    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
 
-    // Vectorul de sortat
-    vector<int> arr = { 3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5 };
+    const int n = 10;
 
-    // Calculează dimensiunea bucăților
-    int block_size = arr.size() / size;
+    // Distribuim datele catre toate procesele
+    vector<int> arr;
+    if (world_rank == 0) {
 
-    // Distribuie bucățile de date către procese
-    vector<int> block(block_size);
-    MPI_Scatter(arr.data(), block_size, MPI_INT, block.data(), block_size, MPI_INT, 0, MPI_COMM_WORLD);
-
-    // Sortează bucățile
-    radix_sort(block);
-
-    // Combinați bucățile de date sortate
-    vector<int> sorted(arr.size());
-    MPI_Gather(block.data(), block_size, MPI_INT, sorted.data(), block_size, MPI_INT, 0, MPI_COMM_WORLD);
-
-    // Procesul 0 afișează vectorul sortat
-    if (rank == 0) {
-        for (int i = 0; i < sorted.size(); i++) {
-            cout << sorted[i] << " ";
+        ifstream fin("inputs/input1.txt");
+        arr.reserve(n);
+        int num;
+        while (fin >> num) {
+            arr.push_back(num);
         }
-        cout << endl;
+        fin.close();
     }
+    vector<int> local_arr(n / world_size);
+    MPI_Scatter(arr.data(), n / world_size, MPI_INT, local_arr.data(), n / world_size, MPI_INT, 0, MPI_COMM_WORLD);
+
+    auto start_time = high_resolution_clock::now();
+    // Sortam vectorii locali folosind algoritmul radix sort
+    radix_sort(local_arr);
+
+    // Colectam datele sortate de la toate procesele
+    vector<int> sorted_arr(n);
+    MPI_Allgather(local_arr.data(), n / world_size, MPI_INT, sorted_arr.data(), n / world_size, MPI_INT, MPI_COMM_WORLD);
+
+    if (world_rank == 0) {
+
+        auto end_time = high_resolution_clock::now();
+        auto duration = duration_cast<microseconds>(end_time - start_time);
+        double total_time = double(duration.count()) / 1000000;
+
+        ofstream foutresult("outputs/output10.out");
+        for (int i = 0; i < n; i++) {
+            foutresult << sorted_arr[i] << endl;
+            cout << sorted_arr[i] << endl;
+        }
+        foutresult.close();
+
+        cout << "Timpul total de executie al functiei de sortare: " << fixed << setprecision(6) << total_time << " secunde" << endl;
+    }
+
     MPI_Finalize();
+
     return 0;
 }
